284cabaeca7dd91f6ee74d46a1d87860f9813a04
Emir Can Marangoz <emircanmarangoz@emirs-macbook-pro.local>
2019-05-10 17:28:54 -0400

    stadium obj

--- voltagePuzzle/voltagePuzzle/GameViewController.swift
+++ voltagePuzzle/voltagePuzzle/GameViewController.swift
@@ -1,37 +1,51 @@
 import UIKit
 import SceneKit
+
 class GameViewController: UIViewController {
+    
     var scnView: SCNView!
     var scnScene: SCNScene!
-    var cameraNode: SCNNode!
+    var game = GameHelper.sharedInstance
+    var verticalCameraNode: SCNNode!
+    var ballNode: SCNNode!
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupView()
+        
         setupScene()
-        setupCamera()
-        scnScene.background.contents = "GeometryFighter.scnassets/Textures/Background_Diffuse.jpg"
+        setupNodes()
+        setupSounds()
     }
-    override var shouldAutorotate: Bool {
-        return true
-    }
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-    func setupView() {
-        scnView = self.view as! SCNView
-    }
+    
     func setupScene() {
-        scnScene = SCNScene()
+        scnView = self.view as! SCNView
+        scnView.delegate = self
+        
+        scnScene = SCNScene(named: "GeometryFighter.scnassets/Scene/Game.scn")
         scnView.scene = scnScene
     }
-    func setupCamera() {
-        // 1
-        cameraNode = SCNNode()
-        // 2
-        cameraNode.camera = SCNCamera()
-        // 3
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 10)
-        // 4
-        scnScene.rootNode.addChildNode(cameraNode)
+    
+    func setupNodes() {
+        
+        verticalCameraNode = scnScene.rootNode.childNode(withName: "camera", recursively: true)!
+        
+        //scnScene.rootNode.addChildNode(game.hudNode)
+        ballNode = scnScene.rootNode.childNode(withName: "Ball", recursively:
+            true)!
+    }
+    
+    func setupSounds() {
+    }
+    
+    override var shouldAutorotate: Bool { return true }
+    
+    override var prefersStatusBarHidden: Bool { return true }
+    
+    // 1
+    
+}
+
+extension GameViewController: SCNSceneRendererDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        game.updateHUD()
     }
 }
